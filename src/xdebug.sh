045f5a09c9ae1761b6e8e33ed8605416b7ad5f2b
Ivan Shumkov <ivan@shumkov.ru>
2016-12-06 20:53:40 +0700

    get remote host form XDEBUG_REMOTE_HOST

--- src/xdebug.sh
+++ src/xdebug.sh
@@ -2,14 +2,8 @@
 
 ARG_XDEBUG="-v"
 ARG_XDEBUG_PARAMS="/dev/null:/etc/php/mods-available/xdebug.ini"
-if [[ ! -z $XDEBUG_CONFIG ]]; then
-    if [[ $(uname) == 'Darwin' ]]; then
-        IP_ADDRESS=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}' | sed "s/addr://" | head -n 1)
-    else
-        IP_ADDRESS="172.17.0.1"
-        #$(ip route list | awk '/default/ {print $3; exit}')
-    fi
 
+if [[ ! -z $XDEBUG_CONFIG ]]; then
     ARG_XDEBUG="-e"
-    ARG_XDEBUG_PARAMS="XDEBUG_CONFIG=$XDEBUG_CONFIG remote_host=$IP_ADDRESS"
+    ARG_XDEBUG_PARAMS="XDEBUG_CONFIG=$XDEBUG_CONFIG remote_host=${XDEBUG_REMOTE_HOST:=172.17.0.1}"
 fi
